e1f8baad022a1f629a5a148585b4d417633862d4
iadelina <adelina.ivan97@gmail.com>
2019-09-11 22:21:19 +0300

    update digital read in soli.py

--- soil.py
+++ soil.py
@@ -4,19 +4,19 @@ import time
  
 #GPIO SETUP
 channel = 21
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
  
 def callback(channel):
         if GPIO.input(channel):
-                print "Water Detected!"
+                print ("No water")
         else:
-                print "Water Detected!"
+                print ("water")
  
-#GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-#GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
  
 # infinite loop
 while True:
-        callback(21)
+        #callback(channel)
         time.sleep(1)
