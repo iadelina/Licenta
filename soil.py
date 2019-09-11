e48975c15af5d22c2c4c74568f47452bce8aa28b
iadelina <adelina.ivan97@gmail.com>
2019-09-11 22:02:24 +0300

    add humidity sensor calculation

--- soil.py
+++ soil.py
@@ -4,7 +4,7 @@ import time
  
 #GPIO SETUP
 channel = 21
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(channel, GPIO.IN)
  
 def callback(channel):
@@ -13,9 +13,10 @@ def callback(channel):
         else:
                 print "Water Detected!"
  
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+#GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+#GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
  
 # infinite loop
 while True:
+        callback(21)
         time.sleep(1)
