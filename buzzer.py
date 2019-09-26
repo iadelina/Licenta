b574ef11d3963dfcfdc2917458cce9e3b878f1dd
iadelina <adelina.ivan97@gmail.com>
2019-09-26 20:15:35 +0300

    BUZZER

--- buzzer.py
+++ buzzer.py
@@ -1,13 +1,16 @@
 #! /usr/bin/python3
 
-import PRi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 BUZZ=14
 GPIO.setup(BUZZ, GPIO.OUT)
+i=0
 
 while True:
-    GPIO.output(BUZZ, GPIO.HIGH)    
-    sleep(1)
-    GPIO.output(BUZZ, GPIO.LOW)
-    sleep(1)
+    while i<3:
+        GPIO.output(BUZZ, GPIO.LOW)
+        sleep(1)
+        GPIO.output(BUZZ, GPIO.HIGH)
+        sleep(1)
+        i+=1
