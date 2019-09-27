6c712f697010dd3748585ce18fe4f766be0ac282
iadelina <adelina.ivan97@gmail.com>
2019-09-27 17:17:47 +0300

    update vibration

--- vibration.py
+++ vibration.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
 
@@ -10,9 +9,9 @@ GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
         if GPIO.input(channel):
-                print "Movement Detected!"
+                print ("Movement Detected!")
         else:
-                print "Movement Detected!"
+                print ("Movement Detected!")
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
