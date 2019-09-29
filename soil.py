c23282265e84a7cbabd3f3cc674de3ec276502ab
adelinai <adelina.ivan97@gmail.com>
2019-09-29 17:49:04 +0300

    soil and adc working

--- soil.py
+++ soil.py
@@ -1,9 +1,21 @@
-#!/usr/bin/python3
-import board
-import busio
-i2c = busio.I2C(board.SCL, board.SDA)
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
-ads = ADS.ADS1115(i2c)
-chan = AnalogIn(ads, ADS.P0)
-print(chan.value, chan.voltage)
+#! /usr/bin/python3
+import time
+import Adafruit_ADS1x15
+
+adc = Adafruit_ADS1x15.ADS1115()
+
+GAIN = 1
+MIN_TRESHOLD = 26421
+MAX_TRESHOLD = 12540
+adc.start_adc(0, gain=GAIN)
+
+print('Reading ADS1x15 channel 0 for 5 seconds...')
+start = time.time()
+while (time.time() - start) <= 100.0:
+    raw = adc.get_last_result()
+    percent = (raw - float(MIN_TRESHOLD))*float(100)/(float(MAX_TRESHOLD) - float(MIN_TRESHOLD))
+    print('Channel 0: {0:.2f} %'.format(percent))
+    print('RAW: {}'.format(raw))
+    time.sleep(0.5)
+adc.stop_adc()
+
---
+++ temperature.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+import Adafruit_DHT as dht
+from time import sleep
+#Set DATA pin
+#BCM 4 => BOARD 7
+DHT = 4
+while True:
+    #Read Temp and Hum from DHT22
+    humidity,temperature = dht.read_retry(dht.DHT22, DHT)
+    #Print Temperature and Humidity on Shell window
+    print('Temp={0:0.1f}*C'.format(temperature))
+    sleep(5)
