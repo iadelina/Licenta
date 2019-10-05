967cea9f326f80580c93d0c4f51da799b76e7f26
adelinai <adelina.ivan97@gmail.com>
2019-10-05 18:27:57 +0300

    ȚȚdefin macros in soli.py

--- soil.py
+++ soil.py
@@ -2,16 +2,21 @@
 import time
 import Adafruit_ADS1x15
 
-adc = Adafruit_ADS1x15.ADS1115()
-
+macro='''
+MAX_VALUE = 100
 GAIN = 1
 MIN_TRESHOLD = 26421
 MAX_TRESHOLD = 12540
+'''
+
+exec(macro)
+
+adc = Adafruit_ADS1x15.ADS1115()
 adc.start_adc(0, gain=GAIN)
 
 print('Reading ADS1x15 channel 0 for 5 seconds...')
 start = time.time()
-while (time.time() - start) <= 100.0:
+while (time.time() - start) <= MAX_VALUE:
     raw = adc.get_last_result()
     percent = (raw - float(MIN_TRESHOLD))*float(100)/(float(MAX_TRESHOLD) - float(MIN_TRESHOLD))
     print('Channel 0: {0:.2f} %'.format(percent))
