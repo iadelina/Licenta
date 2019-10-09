a02fc71ee43af3a8092daaf7c5bdc8cc1706e5b5
adelinai <adelina.ivan97@gmail.com>
2019-10-09 22:20:34 +0300

    add rfid with rfid library install

--- rfid.py
+++ rfid.py
@@ -1,33 +1,16 @@
-#!/usr/bin/python3
-import time
+#! /usr/bin/python3
 import RPi.GPIO as GPIO
-import MFRC522
- 
-# Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
- 
-# Welcome message
-print("Looking for cards")
-print("Press Ctrl-C to stop.")
- 
-# This loop checks for chips. If one is near it will get the UID
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+
 try:
-   
-  while True:
- 
-    # Scan for cards
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
- 
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
- 
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
- 
-      # Print UID
-      print("UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
- 
-      time.sleep(2)
- 
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
 except KeyboardInterrupt:
-  GPIO.cleanup()
+    GPIO.cleanup()
+    raise
