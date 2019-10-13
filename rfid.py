16041e1e481583a6e45123a3ecf91e891b5601ce
adelinai <adelina.ivan97@gmail.com>
2019-10-13 14:00:09 +0300

    rfid working :)

--- rfid.py
+++ rfid.py
@@ -4,13 +4,14 @@ from time import sleep
 import sys
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
-
+key = "1088853604795"
 try:
     while True:
         print("Hold a tag near the reader")
         id, text = reader.read()
-        print("ID: %s\nText: %s" % (id,text))
-        sleep(5)
+        if str(id) == key:
+            print("Door opened!\n")
+        sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
