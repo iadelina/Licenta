5efc111933a17e9445e9f0b88359ea7b077086e5
iadelina <adelina.ivan97@gmail.com>
2019-10-14 20:24:00 +0300

    update with electromagnetic lock

--- rfid.py
+++ rfid.py
@@ -5,12 +5,18 @@ import sys
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 key = "1088853604795"
+RELAY=37
+GPIO.setmode(GPIO.BOARD)
 try:
     while True:
         print("Hold a tag near the reader")
         id, text = reader.read()
         if str(id) == key:
             print("Door opened!\n")
+            GPIO.output(RELAY, HIGH)
+            sleep(2)
+            GPIO.output(RELAY, LOW)
+            sleep(0.5)
         sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
