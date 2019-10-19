29eb2b2090bc70a6262b193617a295393b0327af
iadelina <44256304+iadelina@users.noreply.github.com>
2019-10-19 11:54:14 +0300

    Update rfid.py

--- rfid.py
+++ rfid.py
@@ -6,17 +6,24 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 key = "1088853604795"
 RELAY=37
+counter=1
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(RELAY, GPIO.OUT)
+GPIO.output(RELAY, GPIO.HIGH)
 try:
     while True:
         print("Hold a tag near the reader")
         id, text = reader.read()
         if str(id) == key:
-            print("Door opened!\n")
-            GPIO.output(RELAY, HIGH)
-            sleep(2)
-            GPIO.output(RELAY, LOW)
-            sleep(0.5)
+            if ( counter == 1):
+                print("Door opened!\n ")
+                GPIO.output(RELAY,GPIO.LOW)
+                counter = 2
+            else:
+                if (counter == 2):
+                    print("Door closed!\n")
+                    GPIO.output(RELAY,GPIO.HIGH)
+                    counter = 1
         sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
