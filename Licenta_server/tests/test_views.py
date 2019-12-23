1d0760db6976978fd8b344a227bc97b5363150d3
adelinai <adelina.ivan97@gmail.com>
2019-12-23 16:59:39 +0200

    try view tests+fancy mail content

--- Licenta_server/tests/test_views.py
+++ Licenta_server/tests/test_views.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from interface.views import render_info_page
 from django.urls import reverse
 import datetime
-
+import unittest
+import mock
 
 class ViewsTest(TestCase):
     @classmethod
@@ -19,4 +20,10 @@ class ViewsTest(TestCase):
     def test_view_control_page_url_exists_at_desired_location(self):
         response = self.client.get('/interface/render_control_page')
         self.assertEqual(response.status_code, 302)
+
+   # @mock.patch('interface.led.IndoorLed.get_current_state', return_value='STINS')
+   # def test_view_control_page_returns_expected_led_state(self):
+   #     response = self.client.get('/interface/render_control_page')
+ # with mock.patch("interface.led.IndoorLed.get_current_state") as mock_object:
+       #     mock_object.return_value = 'STINS'
 
