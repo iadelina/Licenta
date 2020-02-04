8473e5cb26fef8a6fbab1dd8173b1c5caa837ca0
adelinai <adelina.ivan97@gmail.com>
2020-02-04 16:48:13 +0200

    rfid reading done

--- Licenta_senzori/rfid.py
+++ Licenta_senzori/rfid.py
@@ -2,19 +2,25 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import sys
+import os
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
-key = "1088853604795"
+#key = "1088853604795"
 RELAY=37
 counter=1
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(RELAY, GPIO.OUT)
 GPIO.output(RELAY, GPIO.HIGH)
-try:
-    while True:
-        print("Hold a tag near the reader")
-        id, text = reader.read()
-        if str(id) == key:
+print("Hold a tag near the reader")
+while True:
+    id, text = reader.read()
+    print('****')
+    file_buffer = open('/home/pi/Desktop/Licenta_latest/Licenta_senzori/keys.txt', 'r+')
+    registered_keys = file_buffer.readlines()
+    print(registered_keys)
+    for key in registered_keys:
+        print(key)
+        if str(id) == key.replace('\n', ''):
             if ( counter == 1):
                 print("Door opened!\n ")
                 GPIO.output(RELAY,GPIO.LOW)
@@ -24,7 +30,8 @@ try:
                     print("Door closed!\n")
                     GPIO.output(RELAY,GPIO.HIGH)
                     counter = 1
-        sleep(0.5)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    raise
+    file_buffer.close()
+    sleep(1)
+#GPIO.cleanup()
+#exit=os.getpid()
+#os.system('kill -9 {}'.format(exit))
