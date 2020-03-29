661110b92c83df7c6da8c499a9d9a2b6b6852b48
adelinai <adelina.ivan97@gmail.com>
2020-03-29 17:19:02 +0300

    updated with working ip

--- env_server_and_celery.sh
+++ env_server_and_celery.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-systemctl stop gunicorn&
-/home/pi/Desktop/environment/bin/python3.5  /home/pi/Desktop/Licenta_latest/Licenta_server/manage.py runserver 192.168.0.15:3001&
+#systemctl stop gunicorn&
+/home/pi/Desktop/environment/bin/python3.5  /home/pi/Desktop/Licenta_latest/Licenta_server/manage.py runserver 192.168.5.1:3000&
 #/home/pi/Desktop/environment/bin/python3.5 fix_path.py&
 cd /home/pi/Desktop/Licenta_latest/Licenta_server
 echo yes | /home/pi/Desktop/environment/bin/celery -A Licenta purge&
